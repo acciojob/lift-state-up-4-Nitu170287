774af333d7c2482d98d63b0daa1eded7964a08d4
Nitu Dubey <nitutiwari01@gmail.com>
2023-07-13 22:45:26 -0400

    completed

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,40 @@
 
-import React from "react";
+import React,{useState} from "react";
 import './../styles/App.css';
+import Child from "./Child";
 
 const App = () => {
+  const[inputItemName, setInputItemName] = useState("")
+  const[inputItemPrice, setInputItemPrice] = useState("")
+  const[items, setItems] = useState([])
+
+  function addItem(){
+    setItems([...items,{name:inputItemName,price:inputItemPrice}])
+    setInputItemName("")
+    setInputItemPrice("")
+  }
+
+  function removeItem(name){
+    console.log("inside remove func")
+    setItems(items.filter((item)=> item.name != name))
+        
+  }
+
   return (
-    <div>
+    <div className="parent">
         {/* Do not remove the main div */}
+        <h1>Parent Component </h1>
+        <label>Item Name</label>
+        <input type="text" name="inputItem" onChange={(e)=>setInputItemName(e.target.value)} value={inputItemName}></input>
+
+        <label>Item Price</label>
+        <input type="number" name="inputItemPrice" onChange={(e)=>setInputItemPrice(e.target.value)} value={inputItemPrice}></input>
+
+        <button onClick={addItem}>Add Item</button>
+
+        
+        <Child itemList={items} removeItem={removeItem}/>
+
     </div>
   )
 }
---
+++ src/components/Child.js
@@ -0,0 +1,22 @@
+import React from "react"
+
+const Child = (props)=>{
+
+    
+
+    return(
+        <div className="child">
+            <h2>Child Component</h2>
+            <ul>
+                {/* {/* <li>Item 1 - $10 <button>Remove</button></li>
+                <li>Item 2 - $20 <button>Remove</button></li> 
+                <li>Item 3 - $30 <button>Remove</button></li> */}
+                {props.itemList.map(item =>(
+                    <li key={item.name}>{item.name + " - $" + item.price}<button onClick={()=>{props.removeItem(item.name)}}>Remove</button></li>
+                ))}
+
+            </ul>
+        </div>
+    )
+}
+export default Child
